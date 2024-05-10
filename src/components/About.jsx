a7cb39895eaa2eb631c16a1604555ddaf6c2a670
wills-dev <ejioguvictor6@gmail.com>
2024-05-11 00:54:29 +0100

    added about  me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="xs:w-[250px] w-fullgreen-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a detail-oriented Frontend Developer with a passion for creating
+        quality applications using JavaScript and its frameworks. My journey
+        began in 2019, after completing a zero-mastery program in web
+        development. Since then, I've gained hands-on experience by contributing
+        to various projects and acquiring certifications in software and web
+        development. <br /> <br /> Proficient in JavaScript, Typescript, Next,
+        React, CSS, HTML, and more, I specialize in crafting interactive user
+        interfaces and dynamic user experiences. I am confident, well-organized,
+        and continuously curious about learning new technologies. I thrive on
+        creative problem-solving and am perpetually working to improve my skills
+        one challenge at a time. <br /> <br /> Outside of coding, you'll often
+        find me on the football field or immersed in video games.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
